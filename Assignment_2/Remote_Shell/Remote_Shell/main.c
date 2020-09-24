8f252f1166250a741165635858cb47c85cb5152a
sumeetm13 <sumeetmaan13@gmail.com>
2020-09-24 14:51:43 -0500

    continous connection client server

--- Assignment_2/Remote_Shell/Remote_Shell/main.c
+++ Assignment_2/Remote_Shell/Remote_Shell/main.c
@@ -18,62 +18,76 @@ int error(char *msg){
     exit(0);
 }
 int main(int argc, char *argv[]){
-  int clientSocket, port_number,n;
-  char buffer[BUFFER_SIZE];
-  struct sockaddr_in serverAddr;
-    struct hostent *server;
-  socklen_t addr_size;
-
-  /*---- Create the socket. The three arguments are: ----*/
-  /* 1) Internet domain 2) Stream socket 3) Default protocol (TCP in this case) */
-    port_number = atoi(argv[2]);
-  clientSocket = socket(AF_INET, SOCK_STREAM, 0);
-  
-    if (clientSocket<0){
-        error('Error opening clients socket');
+    while(1){
+        int clientSocket, port_number,n;
+        char buffer[BUFFER_SIZE];
+        struct sockaddr_in serverAddr;
+        struct hostent *server;
+        socklen_t addr_size;
+        
+        /*---- Create the socket. The three arguments are: ----*/
+        /* 1) Internet domain 2) Stream socket 3) Default protocol (TCP in this case) */
+        port_number = atoi(argv[2]);
+        clientSocket = socket(AF_INET, SOCK_STREAM, 0);
+        
+        if (clientSocket<0){
+            error("Error opening clients socket\n");
+        }
+        
+        server = gethostbyname(argv[1]);
+        if (server==NULL){
+            fprintf(stderr,"No such host \n\n");
+            exit(0);
+        }
+        
+        bzero((char *) &serverAddr, sizeof(serverAddr));
+        /*---- Configure settings of the server address struct ----*/
+        /* Address family = Internet */
+        serverAddr.sin_family = AF_INET;
+        
+        /* Set port number, using htons function to use proper byte order */
+        serverAddr.sin_port = htons(port_number);
+        
+        /* Set IP address to localhost */
+        
+        bcopy((char *)server->h_addr,(char *)&serverAddr.sin_addr.s_addr,server->h_length);
+        /* Set all bits of the padding field to 0 */
+        memset(serverAddr.sin_zero, '\0', sizeof serverAddr.sin_zero);
+        
+        /*---- Connect the socket to the server using the address struct ----*/
+        addr_size = sizeof(serverAddr);
+        
+        if (connect(clientSocket, (struct sockaddr *) &serverAddr, addr_size)<0){
+            error("Error connecting client to server\n");
+        }
+        
+        printf("command please> ");
+        bzero(buffer,BUFFER_SIZE);
+        fgets(buffer,BUFFER_SIZE,stdin);
+        fgets(buffer,BUFFER_SIZE,stderr);
+        fgets(buffer,BUFFER_SIZE,stdout);
+        printf("Before write in client\n");
+        n = write(clientSocket,buffer,strlen(buffer));
+        if (n < 0)
+        {
+            error("ERROR writing to socket\n");
+        }
+        bzero(buffer,BUFFER_SIZE);
+        printf("Before read in client\n");
+        n = read(clientSocket,buffer,BUFFER_SIZE);
+        if (n < 0)
+        {
+            error("ERROR reading from socket\n");
+        }
+        
+        printf("%s\n",buffer);
+        
+        if (strcmp(buffer,"quit\n")==0){
+            printf("Server exited successfully\n");
+            exit(0);
+            break;
+        }
+        close(clientSocket);
     }
-    
-    server = gethostbyname(argv[1]);
-    if (server==NULL){
-        fprintf(stderr,'No such host \n');
-        exit(0);
-    }
-    
-    bzero((char *) &serverAddr, sizeof(serverAddr));
-  /*---- Configure settings of the server address struct ----*/
-  /* Address family = Internet */
-  serverAddr.sin_family = AF_INET;
-
-  /* Set port number, using htons function to use proper byte order */
-  serverAddr.sin_port = htons(port_number);
-
-  /* Set IP address to localhost */
-  
-    bcopy((char *)server->h_addr,(char *)&serverAddr.sin_addr.s_addr,server->h_length);
-  /* Set all bits of the padding field to 0 */
-  memset(serverAddr.sin_zero, '\0', sizeof serverAddr.sin_zero);
-
-  /*---- Connect the socket to the server using the address struct ----*/
-  addr_size = sizeof serverAddr;
-    if (connect(clientSocket, (struct sockaddr *) &serverAddr, addr_size)<0){
-        error('Error connecting client to server');
-    }
-
-    printf("Enter your command: ");
-    bzero(buffer,BUFFER_SIZE);
-    fgets(buffer,BUFFER_SIZE,stdin);
-    n = write(clientSocket,buffer,strlen(buffer));
-    if (n < 0)
-    {
-    error("ERROR writing to socket");
-    }
-    bzero(buffer,BUFFER_SIZE);
-    n = read(clientSocket,buffer,BUFFER_SIZE);
-    if (n < 0)
-    {
-    error("ERROR reading from socket");
-    }
-    printf("%s\n",buffer);
-
     return 0;
 }
--- Assignment_2/Remote_Shell/Remote_Shell/server.c
+++ Assignment_2/Remote_Shell/Remote_Shell/server.c
@@ -15,95 +15,146 @@ void error(char *msg){
     perror(msg);
     exit(1);
 }
+
+void parsing(char inputBuffer[], char *args[])
+{
+    const char s[4] = " \t\n";
+    char *token;
+    token = strtok(inputBuffer, s);
+    int i = 0;
+    while( token != NULL)
+    {
+        args[i] = token;
+        i++;
+        //printf("%s\n", token);
+        token = strtok(NULL,s);
+    }
+    args[i] = NULL;
+    free(token);
+    
+}
 int main(int argc, char *argv[]){
-  int welcomeSocket, newSocket, port_number, client_len;
-  char buffer[BUFFER_SIZE];
-  struct sockaddr_in serverAddr, clientAddr;
+    int welcomeSocket, newSocket, port_number, client_len;
+    char buffer[BUFFER_SIZE];
+    struct sockaddr_in serverAddr, clientAddr;
     char data[MAX];
     data[0] = '\0';
     memset(data,0,MAX);
-  int n;
-  socklen_t addr_size;
-
+    int n;
+    char *args[100];
+    
     
     if (argc<2){
-        fprintf(stderr,'No port number provided');
+        fprintf(stderr,"No port number provided\n");
         exit(1);
     }
-  /*---- Create the socket. The three arguments are: ----*/
-  /* 1) Internet domain 2) Stream socket 3) Default protocol (TCP in this case) */
-  welcomeSocket = socket(AF_INET, SOCK_STREAM, 0);
-  
+    /*---- Create the socket. The three arguments are: ----*/
+    /* 1) Internet domain 2) Stream socket 3) Default protocol (TCP in this case) */
+    welcomeSocket = socket(AF_INET, SOCK_STREAM, 0);
+    
     if (welcomeSocket<0){
-        error('Error while opening socket');
+        error("Error while opening socket\n");
     }
     /*---- Clear serverAddress before using serverAddr ----*/
     bzero((char *) &serverAddr, sizeof(serverAddr));
     port_number = atoi(argv[1]);
     
-  /*---- Configure settings of the server address struct ----*/
-  /* Address family = Internet */
-  serverAddr.sin_family = AF_INET;
-  /* Set port number, using htons function to use proper byte order */
-  serverAddr.sin_port = htons(port_number);
-  /* Set IP address to localhost */
-  serverAddr.sin_addr.s_addr = INADDR_ANY;
-  /* Set all bits of the padding field to 0 */
-  memset(serverAddr.sin_zero, '\0', sizeof serverAddr.sin_zero);  
-
-  /*---- Bind the address struct to the socket ----*/
+    /*---- Configure settings of the server address struct ----*/
+    /* Address family = Internet */
+    serverAddr.sin_family = AF_INET;
+    /* Set port number, using htons function to use proper byte order */
+    serverAddr.sin_port = htons(port_number);
+    /* Set IP address to localhost */
+    serverAddr.sin_addr.s_addr = INADDR_ANY;
+    /* Set all bits of the padding field to 0 */
+    memset(serverAddr.sin_zero, '\0', sizeof serverAddr.sin_zero);
+    
+    /*---- Bind the address struct to the socket ----*/
     if (bind(welcomeSocket, (struct sockaddr *) &serverAddr, sizeof(serverAddr))<0){
-        error('Error on Binding');
+        error("Error on Binding\n");
     }
-
-  /*---- Listen on the socket, with 5 max connection requests queued ----*/
-  if(listen(welcomeSocket,5)==0)
-    printf("Listening\n");
-  else
-    printf("Error\n");
+    
+    /*---- Listen on the socket, with 5 max connection requests queued ----*/
+    if(listen(welcomeSocket,5)==0)
+        printf("Listening\n");
+    else
+        printf("Error\n");
     
     client_len = sizeof(clientAddr);
+    
+    /*---- Accept call creates a new socket for the incoming connection ----*/
+    while(1){
+        printf("Entering while loop\n");
+        newSocket = accept(welcomeSocket, (struct sockaddr *) &clientAddr, &client_len);
+//        dup2(newSocket, STDOUT_FILENO);
+//        dup2(newSocket, STDERR_FILENO);
+        if (newSocket<0){
+            error("Error while accepting\n");
+        }
+        
+        printf("New socket created\n");
+        
+        bzero(buffer, BUFFER_SIZE);
+        /*---- Send message to the socket of the incoming connection ----*/
+        n = read(newSocket,buffer,BUFFER_SIZE);
+        if (n<0){
+            error("Error while reading\n");
+        }
+        
 
-  /*---- Accept call creates a new socket for the incoming connection ----*/
-  
-  newSocket = accept(welcomeSocket, (struct sockaddr *) &clientAddr, &client_len);
-
-    if (newSocket<0){
-        error('Error while accepting');
+        printf("New socket read %s\n", buffer);
+        strcpy(data,  buffer);
+        printf("%s %s\n",data,buffer);
+        if (strcmp(data, "quit\n")==0){
+            printf("You asked me to quit \n");
+            
+            send(newSocket,data,20,0);
+            
+            exit(0);
+            break;
+        } // this will force the code to exit
+           
+        
+        
+        printf("Whats goin on\n");
+        parsing(data,args);
+        printf("Heyya \n");
+        pid_t pid = fork();
+        if (pid >0){//parent process
+            printf("I am in parent\n");
+            wait(NULL);
+            printf("\n");
+            
+            send(newSocket,"Success",20,0);
+            printf("Child completed \n");
+            
+            
+        }
+        
+        else if (pid==0){//child process
+            if (execvp(args[0],args)< 0){
+                error("Could not execvp command\n");
+            }
+            printf("I am in child\n");
+            
+        }
+        else{
+            //pid<0
+            error("Error while forking\n");
+            exit(1);
+        }
+        
+        printf("finishing the socket work\n");
+        
+       
     }
-    bzero(buffer, BUFFER_SIZE);
-  /*---- Send message to the socket of the incoming connection ----*/
-    n = read(newSocket,buffer,BUFFER_SIZE);
-    if (n<0){
-        error('Error while reading');
-    }
-    
-    dup2(newSocket, STDOUT_FILENO);
-    dup2(newSocket, STDERR_FILENO);
-
-    strcpy(data,  buffer);
-    if (strcmp(data, "exit")==0) // this will force the code to exit
-        exit(0);
-    printf("Here is the message: %s\n",buffer);
-    char *args[100];
-    setup(data,args,0);
+    printf("Freeing args\n");
+    close(newSocket);
+    exit(0);
     
     
-    if (execvp(args[0],args)< 0){
-        error('Could not execute command');
-    }
-    
-
-    
-    n = write(newSocket,"I got your message",18);
-    
-
-    if (n < 0)
-    {
-    error("ERROR writing to socket");
-    }
     return 0;
-
-
- 
+    
+    
+    
 }
